bfe7cb19a921852e3ae60d2853ca625cbd574f55
Rodney Dennis <rodneydennisjr@gmail.com>
2016-06-17 13:45:10 -0500

    fix zen mode

--- scripts/main.js
+++ scripts/main.js
@@ -47,8 +47,39 @@ function refresh() {
 
 setInterval(refresh, 1000);
 
-document.body.querySelector('#toggle').addEventListener('click', function() {
+document.addEventListener("keydown", function(e) {
+  if (e.keyCode == 13) {
+    document.body.querySelectorAll('.target').forEach(function(el) {
+      el.classList.toggle("hidden");
+    });
+    toggleFullScreen();
+  }
+}, false);
+
+document.body.querySelector('#zen').addEventListener('click', function() {
   document.body.querySelectorAll('.target').forEach(function(el) {
     el.classList.toggle("hidden");
   });
+  toggleFullScreen();
 });
+
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {
+      document.documentElement.requestFullScreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullScreen) {
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
+}
